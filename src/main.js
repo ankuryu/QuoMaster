90fad82d64aad2ed30791050006c570a2dba5a71
sunil <sunil@vpg>
2018-04-11 16:25:24 +0530

    updated many compo added serverjs

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
+/* jshint  esversion:6 */
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import VueRouter from 'vue-router'
-import vueResource from 'vue-resource'
-import quotations from './components/quotations'
-import About from './components/About'
-import AddQuo from './components/AddQuo'
-import Edit from './components/Edit'
-import QuoteDetails from './components/QuoteDetails'
+import Vue from 'vue';
+import App from './App';
+import VueRouter from 'vue-router';
+import vueResource from 'vue-resource';
+import quotations from './components/quotations';
+import About from './components/About';
+import AddQuo from './components/AddQuo';
+import Edit from './components/Edit';
+import QuoteDetails from './components/QuoteDetails';
 
-Vue.use(vueResource)
-Vue.use(VueRouter)
+Vue.use(vueResource);
+Vue.use(VueRouter);
 
 const router = new VueRouter({
   mode: 'history',
@@ -23,7 +25,7 @@ const router = new VueRouter({
     {path: '/customer/:id', component: QuoteDetails},
     {path: '/edit/:id', component: Edit},
   ]
-})
+});
 
 /* eslint-disable no-new */
 new Vue({
@@ -55,4 +57,4 @@ new Vue({
       <router-view></router-view>
     </div>
   `
-}).$mount('#app')
+}).$mount('#app');
