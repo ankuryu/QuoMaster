7f4dc0586b022f78005f6bc0a13183081f045f06
Ankuryu <pmco89@gmail.com>
2018-05-26 04:48:58 +0530

    more changes to quoitm and tnc

--- src/components/items.vue
+++ src/components/items.vue
@@ -2,19 +2,38 @@
 <div class="itms container">
   <router-link to="/onequo">Back</router-link>
   <router-link to="/oneitem">Edit Item</router-link>
-  <h1 class="page-header">Items of Quotation</h1>
+  <h3 class="page-header">Items of Quotation</h3>
 <table class="table table-striped">
     <thead>
       <tr>
-        <th></th>
-        <th></th>
-        <th>Name</th>
-        <th></th>
+        <th>Sr</th>
+        <th>Type</th>
+        <th>Desc</th>
+        <th>HSN</th>
+        <th>Qty</th>
+        <th>Unt</th>
+        <th>Rate</th>
+        <th>Disc</th>
+        <th>Tax</th>
+        <th>Cost</th>
+        <th>Fact</th>
+        <th>Incr</th>
+        <th>Nar</th>
       </tr>
     </thead>
     <tbody>
 
       <tr v-for="item in items">
+        <td>{{}}</td>
+        <td>{{}}</td>
+        <td>{{}}</td>
+        <td>{{}}</td>
+        <td>{{}}</td>
+        <td>{{}}</td>
+        <td>{{}}</td>
+        <td>{{}}</td>
+        <td>{{}}</td>
+        <td>{{}}</td>
         <td>{{}}</td>
         <td>{{}}</td>
         <td>{{}}</td>
--- src/components/tnc.vue
+++ src/components/tnc.vue
@@ -5,20 +5,35 @@
   <label for="rats">Rates:</label>
 <input type="text" placeholder="Rates:" v-model="rates"/>
 <br>
-  <label for="dlvl">Delivery</label>
-<input type="text" placeholder="Delivery :" v-model="dlvl"/>
+  <label for="dlvtm">Delivery</label>
+<input type="text" placeholder="Delivery :" v-model="dlvtm"/> &nbsp;
+<input type="text" placeholder="Dlvy Units" v-model="dlvunt"/>
 <br>
 <label for="pmt">pmt</label>
-<input type="text" placeholder="Payment : " v-model="pmt" />
+<input type="text" placeholder="Payment : " v-model="pmt" /> &nbsp;
+<input type="text" placeholder="Pmt Unts" v-model="pmtunt" />
 <br>
 <label for="disc">Discount</label>
 <input type="text" placeholder="Discount: " v-model="disc">
 <br>
 <label for="gst">GST</label>
-<input type="text" placeholder="GST : " v-model="gst"/>
+<input type="text" placeholder="GST : " v-model="tax"/>
 <br>
 <label for="vld">Validity</label>
-<input type="text" placeholder="Validity : " v-model="vld"/>
+<input type="text" placeholder="Validity : " v-model="vld"/> &nbsp;
+<input type="text" placeholder="Vld Units" v-model="vldunt" />
+<br>
+<label for="wrt">Warranty</label>
+<input type="text" placeholder="Validity : " v-model="wrt"/>
+<br>
+<label for="nar1">Narration 1</label>
+<input type="text" placeholder="Narration1: " v-model="nar1"/>
+<br>
+<label for="nar2">Narration 2</label>
+<input type="text" placeholder="Narration 2" v-model="nar2"/>
+<br>
+<label for="nar3">Narration 3</label>
+<input type="text" placeholder="Narration 3 " v-model="nar3"/>
 <br>
 <br>
 <button>Save</button> &nbsp; <button>Cancel</button>
@@ -31,13 +46,21 @@ export default {
 	props : '[]',
 	data() {
     return {
+      qno : "",
       rates: "",
-	    dlvl : "" ,
+	    dlvtm : "" ,
+      dlvunt : "",
+      dlvloc : "",
 	    pmt : "",
+      pmtunt: "",
 	    disc : "",
-	    gst : "" ,
+	    tax: "" ,
+      wrt : "",
 	    vld : "" ,
-	    vldunt : ""
+	    vldunt : "",
+      nar1: "",
+      nar2: "",
+      nar3: ""
     }
   }
 }
