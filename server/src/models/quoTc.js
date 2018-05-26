8efc3e38f737a93179a6028655cbb7f020d101a1
sunil <sunil@vpg>
2018-05-26 17:11:13 +0530

    Changes to server files

---
+++ server/src/models/quoTc.js
@@ -0,0 +1,25 @@
+/* JSHINT ES6 VERSION */
+module.exports = (sequelize, DataTypes) => {
+  const Quotc= sequelize.define('quotc', 
+	  {
+	  id: { type: DataTypes.INTEGER,
+		  primaryKey: true 
+	  },
+    qno: DataType.STRING,
+    vld: DataTypes.DECIMAL,
+    vldunt: DataTypes.STRING,
+    pmt: DataTypes.DECIMAL,
+    pmtunt: DataTypes.STRING,
+    dlvtm: DataTypes.DECIMAL,
+    dlvunt: DataTypes.STRING,
+    dlvloc: DataTypes.STRING,
+    tax: DataTypes.DECIMAL,
+    disc: DataTypes.DECIMAL,
+    wrt: DataTypes.STRING,
+    nar: DataTypes.STRING
+     },
+  { freezeTableName: true,
+    timestamps: false
+  }
+  );
+}
